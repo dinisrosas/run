708c6a30c71e7b37b7be77ef4472bbfb51e2a0fe
Dinis Rosas <dinisrosas@hotmail.com>
2024-10-13 18:26:17 +0700

    geo

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,8 @@
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ fetch }) => {
+export const load: PageServerLoad = async ({ fetch, request }) => {
+  console.log(request.headers);
+
   const response = await fetch("/api/geo");
   const geo = await response.json();
   return { geo };
