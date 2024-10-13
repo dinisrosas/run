32ed35f6db66001f7892cf2dd244d37af372632a
Dinis Rosas <dinisrosas@hotmail.com>
2024-10-13 18:36:15 +0700

    headers fix

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,6 +2,5 @@ import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ request }) => {
   console.log(request.headers);
-
-  return { headers: request.headers };
+  return { headers: request.headers.entries() };
 };
